619cf04762b962ac523ae3288c4e830fc308f735
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-07 22:34:14 +0700

    menambahkan navigasi dasar antara halaman Home, Form, dan Detail

---
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -0,0 +1,36 @@
+navigasi.kt
+
+package com.example.navigasitugas
+
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasitugas.view.Awal
+import com.example.navigasitugas.view.TampilData
+import com.example.navigasitugas.view.Form
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail,
+    Homepage,
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Homepage.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        )
